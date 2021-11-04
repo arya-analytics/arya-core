f5797721a2811552656c496ff42ac2444a4e8f87
emiliano bonilla <emilbon99@gmail.com>
2021-11-04 13:34:47 -0400

    removed unnecessary comments

--- cmd/main.go
+++ cmd/main.go
@@ -56,7 +56,7 @@ func newDBCommand(migrations *migrate.Migrations) *cli.Command {
 				},
 			},
 			{
-				Name: "migrate",
+				Name:  "migrate",
 				Usage: "migrate database",
 				Action: func(c *cli.Context) error {
 					core, ctx := aryacore.NewCore(c.Context, aryacore.GetConfig())
@@ -76,13 +76,13 @@ func newDBCommand(migrations *migrate.Migrations) *cli.Command {
 				},
 			},
 			{
-				Name: "rollback",
+				Name:  "rollback",
 				Usage: "rollback the last migration group",
 				Action: func(c *cli.Context) error {
 					core, ctx := aryacore.NewCore(c.Context, aryacore.GetConfig())
 					db := core.ConnManager.GetOrCreate("default").(*bun.DB)
 					migrator := migrate.NewMigrator(db, migrations)
-					group, err := migrator.Rollback(ctx);
+					group, err := migrator.Rollback(ctx)
 					if err != nil {
 						return err
 					}
@@ -94,182 +94,6 @@ func newDBCommand(migrations *migrate.Migrations) *cli.Command {
 					return nil
 				},
 			},
-			//{
-			//	Name:  "migrate",
-			//	Usage: "migrate database",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		group, err := migrator.Migrate(ctx)
-			//		if err != nil {
-			//			return err
-			//		}
-			//
-			//		if group.ID == 0 {
-			//			fmt.Printf("there are no new migrations to run\n")
-			//			return nil
-			//		}
-			//
-			//		fmt.Printf("migrated to %s\n", group)
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:  "rollback",
-			//	Usage: "rollback the last migration group",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		group, err := migrator.Rollback(ctx)
-			//		if err != nil {
-			//			return err
-			//		}
-			//
-			//		if group.ID == 0 {
-			//			fmt.Printf("there are no groups to roll back\n")
-			//			return nil
-			//		}
-			//
-			//		fmt.Printf("rolled back %s\n", group)
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:  "lock",
-			//	Usage: "lock migrations",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//		return migrator.Lock(ctx)
-			//	},
-			//},
-			//{
-			//	Name:  "unlock",
-			//	Usage: "unlock migrations",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//		return migrator.Unlock(ctx)
-			//	},
-			//},
-			//{
-			//	Name:  "create_go",
-			//	Usage: "create Go migration",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		name := strings.Join(c.Args().Slice(), "_")
-			//		mf, err := migrator.CreateGoMigration(ctx, name)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
-			//
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:  "create_sql",
-			//	Usage: "create up and down SQL migrations",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		name := strings.Join(c.Args().Slice(), "_")
-			//		files, err := migrator.CreateSQLMigrations(ctx, name)
-			//		if err != nil {
-			//			return err
-			//		}
-			//
-			//		for _, mf := range files {
-			//			fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
-			//		}
-			//
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:  "status",
-			//	Usage: "print migrations status",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		ms, err := migrator.MigrationsWithStatus(ctx)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		fmt.Printf("migrations: %s\n", ms)
-			//		fmt.Printf("unapplied migrations: %s\n", ms.Unapplied())
-			//		fmt.Printf("last migration group: %s\n", ms.LastGroup())
-			//
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:  "mark_applied",
-			//	Usage: "mark migrations as applied without actually running them",
-			//	Action: func(c *cli.Context) error {
-			//		ctx, app, err := bunapp.StartCLI(c)
-			//		if err != nil {
-			//			return err
-			//		}
-			//		defer app.Stop()
-			//
-			//		migrator := migrate.NewMigrator(app.DB(), migrations)
-			//
-			//		group, err := migrator.Migrate(ctx, migrate.WithNopMigration())
-			//		if err != nil {
-			//			return err
-			//		}
-			//
-			//		if group.ID == 0 {
-			//			fmt.Printf("there are no new migrations to mark as applied\n")
-			//			return nil
-			//		}
-			//
-			//		fmt.Printf("marked as applied %s\n", group)
-			//		return nil
-			//	},
-			//},
 		},
 	}
 }
