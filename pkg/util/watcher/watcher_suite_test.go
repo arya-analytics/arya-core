99f910091463b46f5cc064658c3490bc7f6b0cb4
emiliano bonilla <emilbon99@gmail.com>
2022-01-11 16:17:29 -0500

    AC-35 fixed util test files

--- pkg/util/watcher/watcher_suite_test.go
+++ pkg/util/watcher/watcher_suite_test.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"	. "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 func TestWatcher(t *testing.T) {
