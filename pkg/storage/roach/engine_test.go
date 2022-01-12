0d92db6cddfa186f966482af7bfde05eb5c32fed
emiliano bonilla <emilbon99@gmail.com>
2022-01-12 13:34:50 -0500

    AC-42 cleaned up test cases

--- pkg/storage/roach/engine_test.go
+++ pkg/storage/roach/engine_test.go
@@ -2,6 +2,7 @@ package roach_test
 
 import (
 	"context"
+	"github.com/arya-analytics/aryacore/pkg/storage"
 	"github.com/arya-analytics/aryacore/pkg/storage/roach"
 	"github.com/arya-analytics/aryacore/pkg/storage/stub"
 	"github.com/google/uuid"
@@ -15,40 +16,44 @@ var dummyEngine = &roach.Engine{
 
 var _ = Describe("Engine", func() {
 	Describe("Adapter", func() {
+		var a storage.Adapter
+		BeforeEach(func() {
+			a = dummyEngine.NewAdapter()
+		})
 		Describe("New Adapter", func() {
 			It("Should create a new adapter without error", func() {
-				a := dummyEngine.NewAdapter()
 				Expect(len(a.ID().String())).To(Equal(len(uuid.New().String())))
 			})
 		})
 		Describe("Is Adapter", func() {
 			Context("Adapter is the correct type", func() {
 				It("Should return true", func() {
-					a := dummyEngine.NewAdapter()
 					Expect(dummyEngine.IsAdapter(a)).To(BeTrue())
 				})
 			})
 			Context("Adapter is the incorrect type", func() {
 				It("Should return false", func() {
 					e := &stub.MDEngine{}
-					a := e.NewAdapter()
-					Expect(dummyEngine.IsAdapter(a)).To(BeFalse())
+					ba := e.NewAdapter()
+					Expect(dummyEngine.IsAdapter(ba)).To(BeFalse())
 				})
 			})
 		})
 	})
 	Describe("Migrations", func() {
+		var mErr error
+		var a storage.Adapter
+		BeforeEach(func() {
+			a = dummyEngine.NewAdapter()
+			ctx := context.Background()
+			mErr = dummyEngine.Migrate(ctx, a)
+		})
 		Describe("Init Migrations", func() {
 			It("Should initialize the migrations without error", func() {
-				a := dummyEngine.NewAdapter()
-				ctx := context.Background()
-				err := dummyEngine.Migrate(ctx, a)
-				Expect(err).To(BeNil())
+				Expect(mErr).To(BeNil())
 			})
 			It("Should create all of the tables correctly", func() {
-				a := dummyEngine.NewAdapter()
 				ctx := context.Background()
-				_ = dummyEngine.Migrate(ctx, a)
 				err := dummyEngine.VerifyMigrations(ctx, a)
 				Expect(err).To(BeNil())
 			})
