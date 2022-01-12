f079a2848a80f0801659f0a42c94ecc8d465a706
emiliano bonilla <emilbon99@gmail.com>
2022-01-12 11:14:28 -0500

    AC-36 documented engine

--- pkg/storage/pooler_test.go
+++ pkg/storage/pooler_test.go
@@ -9,10 +9,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var dummyEngines = []storage.Engine{
-	&stub.MDEngine{},
-}
-
 var _ = Describe("Pooler", func() {
 	var p *storage.Pooler
 	BeforeEach(func() {
@@ -20,19 +16,17 @@ var _ = Describe("Pooler", func() {
 		p = storage.NewPooler()
 	})
 	Describe("Retrieving a new adapter", func() {
-		Context("The config was provided", func() {
-			It("Should retrieve an adapter", func() {
-				a, err := p.Retrieve(&stub.MDEngine{})
-				Expect(err).To(BeNil())
-				Expect(len(a.ID().String())).To(Equal(len(uuid.New().String())))
-			})
-			It("Should retrieve the same adapter if queried twice", func() {
-				aOne, err := p.Retrieve(&stub.MDEngine{})
-				Expect(err).To(BeNil())
-				aTwo, err := p.Retrieve(&stub.MDEngine{})
-				Expect(err).To(BeNil())
-				Expect(aOne.ID()).To(Equal(aTwo.ID()))
-			})
+		It("Should retrieve an adapter", func() {
+			a, err := p.Retrieve(&stub.MDEngine{})
+			Expect(err).To(BeNil())
+			Expect(len(a.ID().String())).To(Equal(len(uuid.New().String())))
+		})
+		It("Should retrieve the same adapter if queried twice", func() {
+			aOne, err := p.Retrieve(&stub.MDEngine{})
+			Expect(err).To(BeNil())
+			aTwo, err := p.Retrieve(&stub.MDEngine{})
+			Expect(err).To(BeNil())
+			Expect(aOne.ID()).To(Equal(aTwo.ID()))
 		})
 	})
 })
--- pkg/storage/roach/engine.go
+++ pkg/storage/roach/engine.go
@@ -25,16 +25,30 @@ const (
 
 // || ENGINE ||
 
+// Engine opens connections and execute queries with a roach database.
+// implements the storage.MetaDataEngine interface.
 type Engine struct {
+	// Username for the database. Does not need to be specified if using DriverSQLite.
 	Username string
+	// Password for the database. Does not need to be specified if using DriverSQLite.
 	Password string
+	// Host IP for the database. Does not need to be specified if using DriverSQLite.
 	Host     string
-	Database string
+	// Port to connect to at Host. Does not need to be specified if using DriverSQLite.
 	Port     int
+	// Database to connect to. Does not need to be specified if using DriverSQLite.
+	Database string
+	// Whether to open a TLS connection or not.
+	// Does not need to be specified if using DriverSQLite.
 	UseTLS   bool
+	// Driver is the connection driver used for the roach data store.
+	// Current options are:
+	// DriverPG which connects via the Postgres wire protocol.
+	// DriverSQLite which uses an in memory SQLite database
 	Driver   Driver
 }
 
+// NewAdapter opens a new connection with the data store and returns a storage.Adapter.
 func (e *Engine) NewAdapter() storage.Adapter {
 	a := &adapter{
 		id: uuid.New(),
@@ -44,6 +58,7 @@ func (e *Engine) NewAdapter() storage.Adapter {
 	return a
 }
 
+// IsAdapter checks if the provided adapter is a roach adapter.
 func (e *Engine) IsAdapter(a storage.Adapter) bool {
 	_, ok := e.bindAdapter(a)
 	return ok
@@ -80,6 +95,7 @@ type adapter struct {
 	e  *Engine
 }
 
+// ID implements the storage.Adapter interface.
 func (a *adapter) ID() uuid.UUID {
 	return a.id
 }
