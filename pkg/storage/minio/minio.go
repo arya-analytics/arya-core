e9788383d3948f8fa6bfd5dd29c05b1b0c269212
emiliano bonilla <emilbon99@gmail.com>
2022-01-24 08:14:09 -0500

    AC-37 initial commit

---
+++ pkg/storage/minio/minio.go
@@ -0,0 +1,35 @@
+package minio
+
+import "github.com/arya-analytics/aryacore/pkg/storage"
+
+type Driver int
+
+const (
+	DriverMinIO Driver = iota
+)
+
+type Config struct {
+	Endpoint  string
+	AccessKey string
+	SecretKey string
+	Token     string
+	UseTLS    bool
+	Driver    Driver
+}
+
+type Engine struct {
+	cfg Config
+}
+
+func New(cfg Config) *Engine {
+	return &Engine{cfg}
+}
+
+func (e *Engine) NewAdapter() storage.Adapter {
+	return newAdapter(e.cfg)
+}
+
+func (e *Engine) IsAdapter(a storage.Adapter) bool {
+	_, ok := bindAdapter(a)
+	return ok
+}
