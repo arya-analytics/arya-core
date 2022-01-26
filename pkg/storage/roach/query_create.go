84e547a60d435dab00e5c355a407232914571870
emiliano bonilla <emilbon99@gmail.com>
2022-01-25 19:25:11 -0500

    AC-37 removed pk validator from roach for now

--- pkg/storage/roach/query_create.go
+++ pkg/storage/roach/query_create.go
@@ -3,10 +3,7 @@ package roach
 import (
 	"context"
 	"github.com/arya-analytics/aryacore/pkg/storage"
-	"github.com/arya-analytics/aryacore/pkg/util/model"
-	"github.com/arya-analytics/aryacore/pkg/util/validate"
 	"github.com/uptrace/bun"
-	"reflect"
 )
 
 type createQuery struct {
@@ -41,27 +38,4 @@ func (c *createQuery) Exec(ctx context.Context) error {
 
 func (c *createQuery) validateReq(rm interface{}) {
 	c.catcher.Exec(func() error { return createReqValidator.Exec(rm) })
-}
-
-// |||| VALIDATORS ||||
-var createReqValidator = validate.New([]validate.Func{
-	validatePK,
-})
-
-func validatePK(v interface{}) (err error) {
-	rfl := v.(*model.Reflect)
-	if rfl.IsChain() {
-		for i := 0; i < rfl.ChainValue().Len(); i++ {
-			err = validatePK(rfl.ChainValueByIndex(i))
-		}
-	} else {
-		f := rfl.Value().FieldByName(model.KeyPK)
-		switch f.Kind() {
-		case reflect.Int:
-			if f.Interface() == 0 {
-				err = storage.Error{Type: storage.ErrTypeNoPK}
-			}
-		}
-	}
-	return err
 }
