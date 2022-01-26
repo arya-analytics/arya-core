68c53d25d85da99baa1658f479b7b1b79dd5674a
emiliano bonilla <emilbon99@gmail.com>
2022-01-25 19:37:24 -0500

    AC-37 removed unused code from uuid hook

--- pkg/storage/roach/hook.go
+++ pkg/storage/roach/hook.go
@@ -1,7 +1,6 @@
 package roach
 
 import (
-	"fmt"
 	"github.com/arya-analytics/aryacore/pkg/util/model"
 	"github.com/google/uuid"
 	"reflect"
@@ -11,11 +10,8 @@ import (
 
 func beforeInsertSetUUID(rfl *model.Reflect) *model.Reflect {
 	rfl.ForEach(func(nRfl *model.Reflect, i int) {
-		fldT, ok := nRfl.Type().FieldByName(model.KeyPK)
+		fldT, _ := nRfl.Type().FieldByName(model.KeyPK)
 		fld := nRfl.Value().FieldByName(model.KeyPK)
-		if !ok {
-			panic(fmt.Sprintf("Detected a model with a pk field not named %s", model.KeyPK))
-		}
 		if fldT.Type == reflect.TypeOf(uuid.UUID{}) && fld.IsZero() {
 			fld.Set(reflect.ValueOf(uuid.New()))
 		}
