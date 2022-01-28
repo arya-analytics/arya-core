06643f9373a49b1118f3db64502b6a42a4aae2d7
emiliano bonilla <emilbon99@gmail.com>
2022-01-28 13:22:08 -0500

    AC-45 added more tests to retrieve

--- pkg/storage/redis/query_ts_retrieve_test.go
+++ pkg/storage/redis/query_ts_retrieve_test.go
@@ -2,8 +2,10 @@ package redis_test
 
 import (
 	"github.com/arya-analytics/aryacore/pkg/storage"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"time"
 )
 
 var _ = Describe("QueryTsRetrieve", func() {
@@ -38,5 +40,42 @@ var _ = Describe("QueryTsRetrieve", func() {
 		It("Should retrieve the correct items", func() {
 			Expect(samples).To(HaveLen(3))
 		})
+	})
+	Describe("Retrieving samples from multiple pks", func() {
+		var err error
+		var samples []*storage.ChannelSample
+		var mockSampleTwo *storage.ChannelSample
+		BeforeEach(func() {
+			createMockSamples(3)
+			mockSeriesTwo := &storage.ChannelConfig{
+				Name: "SG_03",
+				ID:   uuid.New(),
+			}
+			if cErrOne := mockEngine.NewTSCreate(mockAdapter).Series().Model(mockSeriesTwo).Exec(
+				mockCtx); cErrOne != nil {
+				panic(cErrOne)
+			}
+			mockSampleTwo = &storage.ChannelSample{
+				Timestamp:       time.Now().UnixNano(),
+				Value:           123.2,
+				ChannelConfigID: mockSeriesTwo.ID,
+			}
+			if cErrTwo := mockEngine.NewTSCreate(mockAdapter).Sample().Model(mockSampleTwo).
+				Exec(mockCtx); cErrTwo != nil {
+				panic(cErrTwo)
+			}
+			err = mockEngine.NewTSRetrieve(mockAdapter).Model(&samples).WherePKs(
+				[]uuid.UUID{mockSeriesTwo.ID, mockSeries.ID}).AllTimeRange().Exec(
+				mockCtx)
+		})
+		It("Should retrieve without error", func() {
+			Expect(err).To(BeNil())
+		})
+		It("Should retrieve the correct items", func() {
+			Expect(samples).To(HaveLen(4))
+		})
+	})
+	Describe("Retrieve samples across a time range", func() {
+
 	})
 })
