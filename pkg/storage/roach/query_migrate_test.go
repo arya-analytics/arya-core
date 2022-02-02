8d9d41aca4bebae9785b6648c331cd0ddbaaf586
emiliano bonilla <emilbon99@gmail.com>
2022-02-02 07:54:17 -0500

    AC-65 fixed some typos in dev tests

--- pkg/storage/roach/query_migrate_test.go
+++ pkg/storage/roach/query_migrate_test.go
@@ -3,13 +3,11 @@ package roach_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	log "github.com/sirupsen/logrus"
 )
 
 var _ = Describe("Migrator", func() {
 	BeforeEach(migrate)
 	Describe("Init Migrations", func() {
-		log.SetReportCaller(true)
 		It("Should create all of the tables correctly", func() {
 			err := mockEngine.NewMigrate(mockAdapter).Verify(mockCtx)
 			Expect(err).To(BeNil())
