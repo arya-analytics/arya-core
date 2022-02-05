8f17ec0e98a26ea88c5757c01fcad26bdab43182
emiliano bonilla <emilbon99@gmail.com>
2022-02-05 12:56:16 -0500

    AC-65 refactored redis tests

--- pkg/storage/redis/query_ts_create_test.go
+++ pkg/storage/redis/query_ts_create_test.go
@@ -2,50 +2,67 @@ package redis_test
 
 import (
 	"github.com/arya-analytics/aryacore/pkg/storage"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"time"
 )
 
 var _ = Describe("QueryTsCreate", func() {
+	var (
+		series *storage.ChannelConfig
+		sample *storage.ChannelSample
+	)
+	JustBeforeEach(func() {
+		err := engine.NewTSCreate(adapter).Series().Model(series).Exec(ctx)
+		Expect(err).To(BeNil())
+	})
 	Describe("Standard Usage", func() {
-		BeforeEach(createMockSeries)
+		BeforeEach(func() {
+			series = &storage.ChannelConfig{
+				Name: "Sensor1",
+				ID:   uuid.New(),
+			}
+		})
 		Describe("Create a new series", func() {
 			It("Should exist after creation", func() {
-				exists, err := mockEngine.NewTSRetrieve(mockAdapter).SeriesExists(mockCtx,
-					mockSeries.ID)
+				exists, err := engine.NewTSRetrieve(adapter).SeriesExists(ctx, series.ID)
 				Expect(err).To(BeNil())
 				Expect(exists).To(BeTrue())
 			})
 		})
 		Describe("Create a new sample", func() {
 			Context("Single sample", func() {
-				It("Should be able to re-retrieve the sample after creation", func() {
-					mockSample := &storage.ChannelSample{
+				JustBeforeEach(func() {
+					err := engine.NewTSCreate(adapter).Sample().Model(sample).Exec(ctx)
+					Expect(err).To(BeNil())
+				})
+				BeforeEach(func() {
+					sample = &storage.ChannelSample{
 						Timestamp:       time.Now().UnixNano(),
 						Value:           123.2,
-						ChannelConfigID: mockSeries.ID,
+						ChannelConfigID: series.ID,
 					}
-					err := mockEngine.NewTSCreate(mockAdapter).Sample().Model(
-						mockSample).Exec(mockCtx)
-					Expect(err).To(BeNil())
-					var samples []*storage.ChannelSample
-					rErr := mockEngine.NewTSRetrieve(mockAdapter).Model(&samples).
-						WherePK(mockSeries.ID).Exec(
-						mockCtx)
+				})
+				It("Should be able to re-retrieve the sample after creation", func() {
+					var resSamples []*storage.ChannelSample
+					rErr := engine.NewTSRetrieve(adapter).Model(&resSamples).
+						WherePK(series.ID).Exec(
+						ctx)
 					Expect(rErr).To(BeNil())
-					Expect(samples).To(HaveLen(1))
-					Expect(samples[0].ChannelConfigID).To(Equal(mockSeries.ID))
-					Expect(samples[0].Value).To(Equal(mockSample.Value))
-					Expect(samples[0].Timestamp).To(Equal(mockSample.Timestamp))
+					Expect(resSamples).To(HaveLen(1))
+					Expect(resSamples[0].ChannelConfigID).To(Equal(series.ID))
+					Expect(resSamples[0].Value).To(Equal(sample.Value))
+					Expect(resSamples[0].Timestamp).To(Equal(sample.Timestamp))
 				})
 			})
 		})
 	})
 	Describe("Edge cases + errors", func() {
+		BeforeEach(func() { series = &storage.ChannelConfig{ID: uuid.New()} })
 		Describe("Not selecting a variant", func() {
 			It("Should return the correct storage error", func() {
-				err := mockEngine.NewTSCreate(mockAdapter).Model(mockSeries).Exec(mockCtx)
+				err := engine.NewTSCreate(adapter).Model(series).Exec(ctx)
 				Expect(err).ToNot(BeNil())
 				Expect(err.(storage.Error).Type).To(Equal(storage.ErrTypeInvalidArgs))
 			})
