652f2958518d59cdd524649e0aa3dda821c9bbe5
emiliano bonilla <emilbon99@gmail.com>
2022-02-08 17:06:28 -0500

    AC-53 hmmmm goroutines are strange in testing

--- pkg/storage/roach/roach_suite_test.go
+++ pkg/storage/roach/roach_suite_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/arya-analytics/aryacore/pkg/storage/mock"
 	"github.com/arya-analytics/aryacore/pkg/storage/roach"
-	log "github.com/sirupsen/logrus"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -19,7 +18,6 @@ var (
 )
 
 var _ = BeforeSuite(func() {
-	log.SetReportCaller(true)
 	migrateErr := engine.NewMigrate(adapter).Exec(ctx)
 	Expect(migrateErr).To(BeNil())
 })
