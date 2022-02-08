959af804cb71ac9e09e728592374a82b0292c76f
emiliano bonilla <emilbon99@gmail.com>
2022-02-08 12:38:21 -0500

    AC-53 simplified tasks

--- pkg/storage/tasks/scheduler.go
+++ pkg/storage/tasks/scheduler.go
@@ -2,66 +2,34 @@ package tasks
 
 import (
 	"context"
-	"github.com/arya-analytics/aryacore/pkg/util/errutil"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
 
-type SchedulerOpt func(opts *schedulerOpts)
-
-func SchedulerWithName(name string) SchedulerOpt {
-	return func(opts *schedulerOpts) {
-		opts.name = name
-	}
-}
-
-type schedulerOpts struct {
-	name string
-}
+// |||| SCHEDULER ||||
 
 type Scheduler struct {
 	Tasks        []Task
 	TickInterval time.Duration
 	Errors       chan error
-	catcher      *errutil.Catcher
 	opts         *schedulerOpts
 }
 
 func NewScheduler(tasks []Task, tickInterval time.Duration, opts ...SchedulerOpt) *Scheduler {
-	errs := make(chan error)
-	catcher := &errutil.Catcher{}
 	s := &Scheduler{
 		Tasks:        tasks,
 		TickInterval: tickInterval,
-		Errors:       errs,
-		catcher:      catcher,
+		Errors:       make(chan error),
 		opts:         &schedulerOpts{},
 	}
 	s.bindOpts(opts...)
 	return s
 }
 
-func (s *Scheduler) logStart() {
-	log.WithFields(log.Fields{
-		"name":       s.opts.name,
-		"task_count": len(s.Tasks),
-		"interval":   s.TickInterval,
-	}).Infof("Starting %s", s.opts.name)
-}
-
-func (s *Scheduler) logTaskFailure(task Task, err error) {
-	log.WithFields(log.Fields{
-		"name":      s.opts.name,
-		"task_name": task.Name,
-	}).Errorf("Task failed! %s", err)
-
-}
-
 func (s *Scheduler) Start(ctx context.Context) {
 	if s.opts.name != "" {
 		s.logStart()
 	}
-
 	t0 := time.Now()
 	t := time.NewTicker(s.TickInterval)
 	defer t.Stop()
@@ -82,20 +50,44 @@ const taskExecThreshold = 20 * time.Millisecond
 func (s *Scheduler) exec(ctx context.Context, t time.Duration) error {
 	for _, task := range s.Tasks {
 		if t%task.Interval < taskExecThreshold {
-			s.catcher.Exec(func() error {
-				if err := task.Action(ctx); err != nil {
-					s.logTaskFailure(task, err)
-					return err
-				}
-				return nil
-			})
+			if err := task.Action(ctx); err != nil {
+				return err
+			}
 		}
 	}
-	return s.catcher.Error()
+	return nil
 }
 
 func (s *Scheduler) bindOpts(opts ...SchedulerOpt) {
 	for _, opt := range opts {
 		opt(s.opts)
 	}
+}
+
+func (s *Scheduler) logStart() {
+	log.WithFields(log.Fields{
+		"name":       s.opts.name,
+		"task_count": len(s.Tasks),
+		"interval":   s.TickInterval,
+	}).Infof("Starting %s", s.opts.name)
+}
+
+func (s *Scheduler) logTaskFailure(task Task, err error) {
+	log.WithFields(log.Fields{
+		"name":      s.opts.name,
+		"task_name": task.Name,
+	}).Errorf("Task failed! %s", err)
+
+}
+
+// |||| SCHEDULER OPTIONS ||||
+
+type SchedulerOpt func(opts *schedulerOpts)
+
+func SchedulerWithName(name string) SchedulerOpt {
+	return func(opts *schedulerOpts) { opts.name = name }
+}
+
+type schedulerOpts struct {
+	name string
 }
