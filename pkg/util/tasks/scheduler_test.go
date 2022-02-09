e098dc61739ab73dbcde4fb4e59fd632d3135a01
emiliano bonilla <emilbon99@gmail.com>
2022-02-09 10:48:02 -0500

    AC-53 increase scheduler test coverage

--- pkg/util/tasks/scheduler_test.go
+++ pkg/util/tasks/scheduler_test.go
@@ -27,6 +27,7 @@ var _ = Describe("SchedulerBase", func() {
 				)
 				go s.Start(ctx)
 				time.Sleep(625 * time.Millisecond)
+				s.Stop()
 				Expect(count).To(Equal(2))
 			})
 			It("Should pipe to the errors channel when a task fails", func() {
@@ -40,7 +41,9 @@ var _ = Describe("SchedulerBase", func() {
 					},
 				}, tasks.ScheduleWithSilence())
 				go s.Start(ctx)
+				time.Sleep(300 * time.Millisecond)
 				Expect(<-s.Errors()).ToNot(BeNil())
+				s.Stop()
 			})
 			It("Should break out of the scheduler when the context is cancelled", func() {
 				ctxWithCancel, cancel := context.WithCancel(ctx)
@@ -76,7 +79,37 @@ var _ = Describe("SchedulerBase", func() {
 					)
 					go s.Start(ctx)
 					time.Sleep(625 * time.Millisecond)
+					s.Stop()
 					Expect(count).To(Equal(12))
+				})
+			})
+			Context("Multiple Tasks", func() {
+				It("Should execute the tasks at the correct interval", func() {
+					countOne, countTwo := 0, 0
+					s := tasks.NewBaseScheduler([]tasks.Task{
+						{
+							Interval: 250 * time.Millisecond,
+							Action: func(ctx context.Context, cfg tasks.SchedulerConfig) error {
+								countOne += 1
+								return nil
+							},
+						},
+						{
+							Interval: 500 * time.Millisecond,
+							Action: func(ctx context.Context, cfg tasks.SchedulerConfig) error {
+								countTwo += 1
+								return nil
+							},
+						},
+					},
+						tasks.ScheduleWithSilence(),
+						tasks.ScheduleWithAccel(5),
+					)
+					go s.Start(ctx)
+					time.Sleep(625 * time.Millisecond)
+					s.Stop()
+					Expect(countOne).To(Equal(12))
+					Expect(countTwo).To(Equal(6))
 				})
 			})
 		})
@@ -91,6 +124,18 @@ var _ = Describe("SchedulerBase", func() {
 							return nil
 						},
 					},
+					{
+						Interval: 300 * time.Millisecond,
+						Action: func(ctx context.Context, cfg tasks.SchedulerConfig) error {
+							return nil
+						},
+					},
+					{
+						Interval: 350 * time.Millisecond,
+						Action: func(ctx context.Context, cfg tasks.SchedulerConfig) error {
+							return nil
+						},
+					},
 				},
 					tasks.ScheduleWithName("test tasks"),
 					tasks.ScheduleWithSilence(),
@@ -98,7 +143,13 @@ var _ = Describe("SchedulerBase", func() {
 				batchScheduler := tasks.NewBatchScheduler(s)
 				go batchScheduler.Start(ctx)
 				time.Sleep(625 * time.Millisecond)
+				var err error
+				go func() {
+					err = <-batchScheduler.Errors()
+				}()
+				batchScheduler.Stop()
 				Expect(count).To(Equal(2))
+				Expect(err).To(BeNil())
 			})
 		})
 	})
