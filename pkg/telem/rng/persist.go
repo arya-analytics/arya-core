d43ed8cf3811d097d06c4e335930af1d14db9849
emiliano bonilla <emilbon99@gmail.com>
2022-02-22 11:02:34 -0500

    AC-103 simplified context catcher

--- pkg/telem/rng/persist.go
+++ pkg/telem/rng/persist.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/arya-analytics/aryacore/pkg/cluster"
 	"github.com/arya-analytics/aryacore/pkg/models"
+	"github.com/arya-analytics/aryacore/pkg/util/errutil"
 )
 
 type Persist interface {
@@ -15,25 +16,13 @@ type PersistCluster struct {
 }
 
 func (p *PersistCluster) NewRange(ctx context.Context, nodeID int) (*models.Range, error) {
+	c := errutil.NewContextCatcher(ctx)
 	r := &models.Range{}
-	if err := p.Cluster.NewCreate().Model(r).Exec(ctx); err != nil {
-		return nil, err
-	}
-	rr := &models.RangeReplica{
-		RangeID: r.ID,
-		NodeID:  nodeID,
-	}
-	if err := p.Cluster.NewCreate().Model(rr).Exec(ctx); err != nil {
-		return nil, err
-	}
-	lease := &models.RangeLease{
-		RangeID:        r.ID,
-		RangeReplicaID: rr.ID,
-		RangeReplica:   rr,
-	}
-	if err := p.Cluster.NewCreate().Model(lease).Exec(ctx); err != nil {
-		return nil, err
-	}
+	c.Exec(p.Cluster.NewCreate().Model(r).Exec)
+	rr := &models.RangeReplica{RangeID: r.ID, NodeID: nodeID}
+	c.Exec(p.Cluster.NewCreate().Model(rr).Exec)
+	lease := &models.RangeLease{RangeID: r.ID, RangeReplicaID: rr.ID, RangeReplica: rr}
+	c.Exec(p.Cluster.NewCreate().Model(lease).Exec)
 	r.RangeLease = lease
 	return r, nil
 }
