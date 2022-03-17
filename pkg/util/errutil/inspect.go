1301fab81166eb6c8d41b0ba1cebe7b0a8465143
emiliano bonilla <emilbon99@gmail.com>
2022-03-17 11:03:12 -0400

    AC-118 adding what I forgot to commit

--- pkg/util/errutil/inspect.go
+++ pkg/util/errutil/inspect.go
@@ -1,3 +1,13 @@
 package errutil
 
-func 
+import (
+	log "github.com/sirupsen/logrus"
+	"reflect"
+)
+
+func Inspect(err error) (error, bool) {
+	log.WithFields(log.Fields{
+		"type": reflect.TypeOf(err),
+	}).Infof("Inspection of error -> %s", err)
+	return err, false
+}
