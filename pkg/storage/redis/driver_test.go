0bcd27dfdb131ef7a959176273d271ef95e3c2e3
emiliano bonilla <emilbon99@gmail.com>
2022-03-16 23:22:27 -0400

    AC-118 redis driver now has full cov

---
+++ pkg/storage/redis/driver_test.go
@@ -0,0 +1,44 @@
+package redis_test
+
+import (
+	"github.com/arya-analytics/aryacore/pkg/models"
+	"github.com/arya-analytics/aryacore/pkg/storage"
+	"github.com/arya-analytics/aryacore/pkg/storage/redis"
+	"github.com/arya-analytics/aryacore/pkg/util/query"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
+)
+
+var _ = Describe("Driver", func() {
+	BeforeEach(func() {
+		viper.SetConfigFile("./testdata/config.json")
+		Expect(viper.ReadInConfig()).To(BeNil())
+	})
+	Describe("Config", func() {
+		It("Should load the viper config correctly", func() {
+			cfg := redis.Config{}.Viper()
+			Expect(cfg.Host).To(Equal("redistsfake"))
+		})
+	})
+	Describe("Connection errors", func() {
+		It("Should return the correct query error", func() {
+			pool := storage.NewPool()
+			cfg := redis.Config{}.Viper()
+			driver := &redis.DriverRedis{Config: cfg}
+			engine := redis.New(driver, pool)
+			err := engine.NewTSRetrieve().Model(&models.ChannelSample{}).WherePK(uuid.New()).Exec(ctx)
+			Expect(err).ToNot(BeNil())
+			Expect(err.(query.Error).Type).To(Equal(query.ErrorTypeConnection))
+		})
+	})
+	Describe("DemandCap", func() {
+		It("Should return the correct demand cap", func() {
+			cfg := redis.Config{}.Viper()
+			driver := &redis.DriverRedis{Config: cfg}
+			Expect(driver.DemandCap()).To(Equal(500))
+		})
+	})
+
+})
