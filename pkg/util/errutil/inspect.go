aedf69bb44726e4e56446c6486e695f3c3049711
emiliano bonilla <emilbon99@gmail.com>
2022-03-17 14:27:59 -0400

    AC-118 documented errutil

--- pkg/util/errutil/inspect.go
+++ pkg/util/errutil/inspect.go
@@ -5,6 +5,9 @@ import (
 	"reflect"
 )
 
+// Inspect logs the following information about an error:
+//		1. Its type :)
+// Adding more soon.
 func Inspect(err error) (error, bool) {
 	log.WithFields(log.Fields{
 		"type": reflect.TypeOf(err),
