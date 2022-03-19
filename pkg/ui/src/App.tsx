312ad523c7a241f1dd04afeeec81877e0366caa2
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2022-03-19 00:31:27 -0400

    Ac 124 admin boiler (#33)
    * AC-124 initial commit of react app
    
    * AC-124 added antd and less support
    
    * AC-124 added theme less file
    
    * AC-124 added jest
    
    Co-authored-by: emiliano bonilla <emilbon99@gmail.com>

---
+++ pkg/ui/src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "antd";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Arya Core UI</h1>
+                <Button type="primary">Get Started</Button>
+            </header>
+        </div>
+    )
+}
+
+export default App
