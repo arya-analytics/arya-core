5b9df718ae84a251c607aae28395fe7e06dba735
emiliano bonilla <emilbon99@gmail.com>
2022-03-21 15:52:24 -0400

    AC-128 added channel support for reflection

--- pkg/util/query/stream_retrieve.go
+++ pkg/util/query/stream_retrieve.go
@@ -1,1 +1,25 @@
 package query
+
+type StreamRetrieve struct {
+	where
+}
+
+func (sr *StreamRetrieve) Model(m interface{}) *StreamRetrieve {
+	sr.baseModel(m)
+	return sr
+}
+
+func (sr *StreamRetrieve) WherePK(pk interface{}) *StreamRetrieve {
+	sr.wherePK(pk)
+	return sr
+}
+
+func (sr *StreamRetrieve) WherePKs(pks interface{}) *StreamRetrieve {
+	sr.wherePKs(pks)
+	return sr
+}
+
+func (sr *StreamRetrieve) BindExec(e Execute) *StreamRetrieve {
+	sr.baseBindExec(e)
+	return sr
+}
