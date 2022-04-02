a3097b2ea04a1fb4108ca94bec16fd802ee73a45
emiliano bonilla <emilbon99@gmail.com>
2022-04-02 18:55:12 -0400

    AC-128 moved local stream create into own file

--- pkg/cluster/chanstream/local.go
+++ pkg/cluster/chanstream/local.go
@@ -31,27 +31,11 @@ func NewLocalStorage(qe query.Execute) *LocalStorage {
 
 func (ls *LocalStorage) exec(ctx context.Context, p *query.Pack) error {
 	return query.Switch(ctx, p, query.Ops{
-		&streamq.TSCreate{}:   ls.create,
+		&streamq.TSCreate{}:   newLocalStreamCreate(ls.qe).exec,
 		&streamq.TSRetrieve{}: newLocalStreamRetrieve(ls.delta).exec,
 		&query.Create{}:       ls.qe,
 		&query.Delete{}:       ls.qe,
 		&query.Retrieve{}:     ls.qe,
 		&query.Delete{}:       ls.qe,
 	})
-}
-
-func (ls *LocalStorage) create(ctx context.Context, p *query.Pack) error {
-	streamQ := stream(p)
-	streamQ.Segment(func() {
-		for {
-			sample, sampleOK := p.Model().ChanRecv()
-			if !sampleOK {
-				break
-			}
-			if err := streamq.NewTSCreate().Model(sample).BindExec(ls.qe).Exec(ctx); err != nil {
-				streamQ.Errors <- err
-			}
-		}
-	})
-	return nil
 }
---
+++ pkg/cluster/chanstream/local_stream_create.go
@@ -0,0 +1,29 @@
+package chanstream
+
+import (
+	"context"
+	"github.com/arya-analytics/aryacore/pkg/models"
+	"github.com/arya-analytics/aryacore/pkg/util/query"
+	"github.com/arya-analytics/aryacore/pkg/util/query/streamq"
+)
+
+type localStreamCreate struct {
+	qe query.Execute
+}
+
+func newLocalStreamCreate(qe query.Execute) *localStreamCreate {
+	return &localStreamCreate{qe: qe}
+}
+
+func (lsc *localStreamCreate) exec(ctx context.Context, p *query.Pack) error {
+	sampleStream := *query.ConcreteModel[*chan *models.ChannelSample](p)
+	streamQ := stream(p)
+	streamQ.Segment(func() {
+		for s := range sampleStream {
+			if err := streamq.NewTSCreate().Model(s).BindExec(lsc.qe).Exec(ctx); err != nil {
+				streamQ.Errors <- err
+			}
+		}
+	})
+	return nil
+}
