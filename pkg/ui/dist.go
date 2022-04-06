a476c1709b6a3c6fa07015a4fae1385dea24b9d0
emiliano bonilla <emilbon99@gmail.com>
2022-04-06 00:43:36 -0400

    AC-126 configured production UI build

--- pkg/ui/dist.go
+++ pkg/ui/dist.go
@@ -2,7 +2,27 @@ package ui
 
 import (
 	"embed"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"io/fs"
+	"net/http"
 )
 
 //go:embed dist/*
-var dist embed.FS
+var _dist embed.FS
+
+const distRoot = "dist"
+
+func distFS() fs.FS {
+	assets, err := fs.Sub(_dist, distRoot)
+	if err != nil {
+		panic(err)
+	}
+	return assets
+}
+
+func distHandler() fiber.Handler {
+	return filesystem.New(filesystem.Config{
+		Root: http.FS(distFS()),
+	})
+}
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -1,15 +1,17 @@
 package ui
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
+// Server servers the UI and assets related to it.
 type Server struct{}
 
+// NewServer creates a new server.
 func NewServer() *Server {
 	return &Server{}
 }
 
-const distRoot = "dist"
-
 func (s *Server) BindTo(router fiber.Router) {
-	router.Static("/", distRoot)
+	router.Use(distHandler())
 }
